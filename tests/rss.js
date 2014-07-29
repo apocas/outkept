afe630e1a876b8415ff26ab3797d4d51213170e8
Pedro Dias <petermdias@gmail.com>
2014-07-29 19:15:48 +0100

    feeds working

--- tests/rss.js
+++ tests/rss.js
@@ -2,25 +2,43 @@ var FeedParser = require('feedparser'),
   request = require('request');
 
 /*
-request('http://rss.phishtank.com/rss/asn/?asn=24768')
+request('http://rss.phishtank.com/rss/asn/?asn=12345')
   .pipe(new FeedParser())
 
   .on('error', function(error) {
-    // always handle errors
+    console.log(error);
   })
-  .on('meta', function (meta) {
-    // do something
-  })
-  .on('article', function (article) {
-    // do something else
-    console.log(article.source);
+  .on('readable', function() {
+    // This is where the action is!
+    var stream = this
+      , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+      , item;
+
+    while (item = stream.read()) {
+      console.log(item.source.title);
+    }
   })
   .on('end', function () {
    // do the next thing
   });
-  */
+*/
 
-  request('http://www.zone-h.org/rss/specialdefacements').pipe(new FeedParser()).on('article', function (article) {
-    // do something else
-    console.log(article['title']);
+request('http://www.zone-h.org/rss/specialdefacements')
+  .pipe(new FeedParser())
+
+  .on('error', function(error) {
+    console.log(error);
+  })
+  .on('readable', function() {
+    // This is where the action is!
+    var stream = this
+      , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+      , item;
+
+    while (item = stream.read()) {
+      console.log(item['rss:title']['#']);
+    }
+  })
+  .on('end', function () {
+   // do the next thing
   });
